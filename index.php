db021cb607b77b8e116b3e808b1b903c87c37c7d
blekerfeld <thomas@germanics.org>
2015-07-12 13:33:46 +0200

    the raw start of things
    Added index.php, au_config.php, some folders already and the favicon

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+|| Aulis
+|| Organisation:	Aulis International
+|| Website:			http://germanics.org/aulis
+|| Developed by: 	Robert Monden
+					Thomas de Roo
+|| License: 		MIT
+|| Version: 		0.01
+|| * File information * 
+||		-> index.php
+| 		-> // This file coördinates the whole dipslay of the site.
+|| 		-> Last change: July, 2015
+*/
+
+?>
