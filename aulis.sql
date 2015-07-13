7277184a097da5b70c2f5a60ad1991b0b125d332
blekerfeld <thomas@germanics.org>
2015-07-13 18:49:10 +0200

    ! PLEASE UPDATE THE DATEBASE TO THE NEWEST aulis.sql

--- aulis.sql
+++ aulis.sql
@@ -5,7 +5,78 @@ SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `aulis_questions`;
+CREATE TABLE `aulis_blog_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `aulis_blog_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) NOT NULL,
+  `logged` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `aulis_blog_entries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `blog_intro` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `blog_content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `blog_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `blog_poster` int(11) NOT NULL,
+  `blog_activated` int(11) NOT NULL,
+  `blog_featured` int(11) NOT NULL,
+  `blog_in_queue` int(11) NOT NULL,
+  `blog_can_comment` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `aulis_blog_queue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) NOT NULL,
+  `publish_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `aulis_blog_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `aulis_blog_tags_entries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entry_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `entry_id` (`entry_id`),
+  KEY `tag_id` (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `aulis_core` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `request` varchar(255) NOT NULL,
+  `core` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `aulis_core` (`id`, `request`, `core`) VALUES
+(1,	'blog',	'ViewBlog'),
+(2,	'topic',	'ViewTopic'),
+(3,	'blogindex',	'BlogIndex'),
+(4,	'board',	'ViewBoard');
+
 CREATE TABLE `aulis_questions` (
   `question_id` int(11) NOT NULL AUTO_INCREMENT,
   `question_title` text NOT NULL,
@@ -19,7 +90,6 @@ CREATE TABLE `aulis_questions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `aulis_sessions`;
 CREATE TABLE `aulis_sessions` (
   `session_id` char(40) NOT NULL,
   `session_data` longtext NOT NULL,
@@ -30,7 +100,6 @@ CREATE TABLE `aulis_sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `aulis_settings`;
 CREATE TABLE `aulis_settings` (
   `id_setting` int(11) NOT NULL AUTO_INCREMENT,
   `setting_name` text NOT NULL,
@@ -42,9 +111,10 @@ CREATE TABLE `aulis_settings` (
 
 INSERT INTO `aulis_settings` (`id_setting`, `setting_name`, `setting_value`) VALUES
 (1,	'theme',	'hannover'),
-(2,	'site_title',	'Aulis');
+(2,	'site_title',	'Aulis'),
+(3,	'language',	'English'),
+(4,	'default_core',	'blogindex');
 
-DROP TABLE IF EXISTS `aulis_users`;
 CREATE TABLE `aulis_users` (
   `user_id` int(11) NOT NULL,
   `user_username` tinytext NOT NULL,
@@ -65,4 +135,4 @@ CREATE TABLE `aulis_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2015-07-13 01:16:12
+-- 2015-07-13 18:48:12
