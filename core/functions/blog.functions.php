71c6da997f6e5a759dd4fddec02f4fefc2203cab
blekerfeld <thomas@germanics.org>
2015-07-15 23:21:43 +0200

    + blog functions (just a start)

--- core/functions/blog.functions.php
+++ core/functions/blog.functions.php
@@ -17,3 +17,25 @@
 if(!defined('aulis'))
 	header("Location: index.php");
 
+// This function will check whether or not we have a requested blog entry aboard
+function au_exist_blog_entry($entry_id){
+
+	// Try to get the entry from the database
+	$entry = au_query("SELECT id FROM blog_entries WHERE id = " . $entry_id . " LIMIT 1;");
+
+	// This function was quick, we are done already
+	return ($entry->rowCount() === 1);
+
+}
+
+// This function gets and returns a blog entry from the database
+function au_get_blog_entry($entry_id){
+
+	// We can only get an item that exists, though
+	if(!au_get_blog_entry($entry_id))
+		return false;
+
+	// Return database object
+	return au_query("SELECT * FROM blog_entries WHERE id = " . $entry_id . " LIMIT 1;");
+
+}
