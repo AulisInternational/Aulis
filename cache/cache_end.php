2e5d1b99fbcfd567dea9689c9df2fd1a4f308ea5
blekerfeld <thomas@germanics.org>
2015-07-23 22:22:16 +0200

    Revert "Output caching works :+1:"
    This reverts commit b0322fa0456c295d14a238fd04933561ae26c942.

--- cache/cache_end.php
+++ cache/cache_end.php
@@ -14,17 +14,3 @@
 */
 
 // The caching feature is yet to come, but this file will end it all.
-
-// If output buffering has been started we need to save it to the cache
-if(ob_get_status()){
-
-	// They need to know the file will be loaded from cache in the future
-	$contents = "<!--FILE WAS LOADED FROM CACHE-->\n" . ob_get_contents();
-
-	// We need to write to cache
-	file_put_contents($cache_file, $contents);
-
-	// End the output buffering
-	ob_end_flush();
-
-}
